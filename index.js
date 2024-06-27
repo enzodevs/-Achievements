802a9b1aed44b8d0d57fa6626ae30d4475825ef5
Enzo Lopes Cambraia Baptista <223335@facens.br>
2024-06-27 11:42:35 -0300

    Update index.js
    Index updated with randomnumber.
    
    Co-Authored-By: Enzo <142342836+spyenzo@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 console.log("Index Js Created")
+
+// Generate a random number between 1 and 10
+const randomNumber = Math.floor(Math.random() * 10) + 1;
+
+// Check if the number is even or odd
+if (randomNumber % 2 === 0) {
+    console.log(`${randomNumber} is an even number.`);
+} else {
+    console.log(`${randomNumber} is an odd number.`);
+}
+
+// Create an array of colors
+const colors = ['red', 'blue', 'green', 'yellow', 'orange'];
+
+// Randomly select a color from the array
+const randomColor = colors[Math.floor(Math.random() * colors.length)];
+
+console.log(`The randomly selected color is ${randomColor}.`);
